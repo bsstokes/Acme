bf629a70dc69991235ea717a7ab692d07510f43d
Brian Stokes <bsstokes@gmail.com>
2023-03-29 10:58:37 -0500

    Tweak assignments layout

--- app/src/main/java/com/bsstokes/acme/assignments/list/AssignmentsListScreen.kt
+++ app/src/main/java/com/bsstokes/acme/assignments/list/AssignmentsListScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,7 +46,10 @@ fun AssignmentsListScreen(
             .fillMaxSize()
             .testTag(AssignmentsListScreenTags.content),
     ) {
-        Text(uiState.title)
+        Text(
+            text = uiState.title,
+            modifier = Modifier.align(Alignment.Center),
+        )
     }
 }
 
