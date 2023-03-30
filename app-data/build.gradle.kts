36cdc5320436bd918586aa97289d36cd6ada478f
Brian Stokes <bsstokes@gmail.com>
2023-03-29 22:05:45 -0500

    Add Kotlin serialization

--- app-data/build.gradle.kts
+++ app-data/build.gradle.kts
@@ -1,10 +1,13 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     kotlin("jvm")
+    alias(libs.plugins.kotlin.serialization)
 }
 
 dependencies {
     implementation(projects.appDomain)
     implementation(libs.coroutines.core)
+    implementation(libs.kotlinx.serialization.json)
 
     testImplementation(projects.testUtils)
     testImplementation(libs.coroutines.test)
