953af5d11bff408dba2e829adfccf92ad1e1ba57
Brian Stokes <bsstokes@gmail.com>
2023-03-31 11:46:16 -0500

    Implement base score

--- algorithm/src/main/kotlin/com/bsstokes/acme/algorithm/SuitabilityScore.kt
+++ algorithm/src/main/kotlin/com/bsstokes/acme/algorithm/SuitabilityScore.kt
@@ -1,8 +1,28 @@
 package com.bsstokes.acme.algorithm
 
+import com.bsstokes.acme.algorithm.math.isEven
+import com.bsstokes.acme.algorithm.words.numberOfConsonants
+import com.bsstokes.acme.algorithm.words.numberOfVowels
+
+/**
+ * Calculate the suitability score (SS) according the prompt:
+ *
+ * * If the length of the shipment's destination street name is even,
+ *   the base suitability score (SS) is the number of vowels in the driver’s name multiplied by 1.5.
+ * * If the length of the shipment's destination street name is odd,
+ *   the base SS is the number of consonants in the driver’s name multiplied by 1.
+ * * If the length of the shipment's destination street name shares any common factors (besides 1)
+ *   with the length of the driver’s name, the SS is increased by 50% above the base SS.
+ */
 internal fun suitabilityScore(
     driverName: String,
     shipmentAddress: String,
 ): Double {
-    return 0.0
+    val base = if (shipmentAddress.length.isEven) {
+        1.5 * driverName.numberOfVowels
+    } else {
+        1.0 * driverName.numberOfConsonants
+    }
+
+    return base
 }
--- algorithm/src/test/kotlin/com/bsstokes/acme/algorithm/SuitabilityScoreKtTest.kt
+++ algorithm/src/test/kotlin/com/bsstokes/acme/algorithm/SuitabilityScoreKtTest.kt
@@ -1,12 +1,29 @@
 package com.bsstokes.acme.algorithm
 
+import com.bsstokes.acme.algorithm.words.numberOfConsonants
+import com.bsstokes.acme.algorithm.words.numberOfVowels
 import com.bsstokes.acme.test.shouldEqual
 import org.junit.Test
 
 class SuitabilityScoreKtTest {
 
     @Test
-    fun `suitabilityScore test`() {
-        suitabilityScore(driverName = "D", shipmentAddress = "S1") shouldEqual 0.0
+    fun `suitabilityScore should match specs for odd length shipping address`() {
+        // "Driver" has 4 consonants and 2 vowels
+        val driverName = "Driver"
+        suitabilityScore(
+            driverName = driverName,
+            shipmentAddress = "1",
+        ) shouldEqual driverName.numberOfConsonants * 1.0
+    }
+
+    @Test
+    fun `suitabilityScore should match specs for even length shipping address`() {
+        // "Driver" has 4 consonants and 2 vowels
+        val driverName = "Driver"
+        suitabilityScore(
+            driverName = driverName,
+            shipmentAddress = "12",
+        ) shouldEqual driverName.numberOfVowels * 1.5
     }
 }
