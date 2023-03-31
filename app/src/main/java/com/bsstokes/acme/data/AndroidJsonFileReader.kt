16b5e4ba26baf02a3486163dc3107bf9d978f6e2
Brian Stokes <bsstokes@gmail.com>
2023-03-31 09:44:02 -0500

    Add a comment for clarity

--- app/src/main/java/com/bsstokes/acme/data/AndroidJsonFileReader.kt
+++ app/src/main/java/com/bsstokes/acme/data/AndroidJsonFileReader.kt
@@ -5,6 +5,11 @@ import com.bsstokes.acme.R
 import com.bsstokes.acme.app.data.repository.JsonFileReader
 import java.io.InputStream
 
+/**
+ * A [JsonFileReader] that reads a JSON file via an Android raw resource.
+ *
+ * Be sure to run this in the background, e.g. with `Dispatchers.IO`, since it might take a while.
+ */
 class AndroidJsonFileReader(
     private val context: Context,
 ) : JsonFileReader {
